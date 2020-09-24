4404d19badd2d157a8feb2cf81a024837c11c1df
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-24 23:07:04 +0300

    Create Coffee.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ CoffeeOrder/Models/Coffee.swift
@@ -0,0 +1,27 @@
+//
+//  Coffee.swift
+//  CoffeeOrder
+//
+//  Created by Abdelrahman Mohamed on 24.09.2020.
+//
+
+import Foundation
+
+struct Coffee {
+    
+    let name: String
+    let imageURL: String
+    let price: Double
+}
+
+extension Coffee {
+    
+    static func all() -> [Coffee] {
+        
+        [
+            Coffee(name: "Cappuccino", imageURL: "Cappuccino", price: 2.5),
+            Coffee(name: "Espresso", imageURL: "Espresso", price: 2.1),
+            Coffee(name: "Regular", imageURL: "Regular", price: 1.5)
+        ]
+    }
+}
