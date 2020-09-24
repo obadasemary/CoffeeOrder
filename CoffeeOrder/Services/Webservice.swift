c1a77aef26656feb616d4e42d73e1228d6c69c92
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-25 02:32:18 +0300

    Add createCoffeeOrder
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- CoffeeOrder/Services/Webservice.swift
+++ CoffeeOrder/Services/Webservice.swift
@@ -9,12 +9,49 @@ import Foundation
 
 class Webservice {
     
-    let path = "https://island-bramble.glitch.me/"
-    let orders = "orders"
+    var path = "https://island-bramble.glitch.me/"
+    var ordersPath = "orders"
+    var fullPath: String
+    
+    
+    init() {
+        
+        fullPath = path + ordersPath
+    }
+    
+    func createCoffeeOrder(order: Order, completion: @escaping (CreateOrderResponse?) -> ()) {
+        
+        guard let url = URL(string: fullPath) else {
+            fatalError("Invalid URL")
+        }
+        
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONEncoder().encode(order)
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            guard let data = data, error == nil else {
+                
+                DispatchQueue.main.async {
+                    
+                    completion(nil)
+                }
+                return
+            }
+            
+            let createOrderResponse = try? JSONDecoder().decode(CreateOrderResponse.self, from: data)
+            DispatchQueue.main.async {
+                
+                completion(createOrderResponse)
+            }
+            
+        }.resume()
+    }
     
     func getAllOrders(completion: @escaping ([Order]?) -> ()) {
-        
-        let fullPath = path + orders
         
         guard let url = URL(string: fullPath) else {
             
