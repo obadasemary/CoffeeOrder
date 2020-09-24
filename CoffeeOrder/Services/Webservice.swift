668a659e22a6adf45e293b68359630018da4e147
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-24 23:54:28 +0300

    Create Webservice.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ CoffeeOrder/Services/Webservice.swift
@@ -0,0 +1,47 @@
+//
+//  Webservice.swift
+//  CoffeeOrder
+//
+//  Created by Abdelrahman Mohamed on 24.09.2020.
+//
+
+import Foundation
+
+class Webservice {
+    
+    let path = "https://candle-bouncy-observation.glitch.me"
+    let orders = "orders"
+    
+    func getAllOrders(completion: @escaping ([Order]?) -> ()) {
+        
+        let fullPath = path + orders
+        
+        guard let url = URL(string: fullPath) else {
+            
+            DispatchQueue.main.async {
+                
+                completion(nil)
+            }
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            
+            guard let data = data, error == nil else {
+                
+                DispatchQueue.main.async {
+                    
+                    completion(nil)
+                }
+                return
+            }
+            
+            let orders = try? JSONDecoder().decode([Order].self, from: data)
+            DispatchQueue.main.async {
+                
+                completion(orders)
+            }
+        }.resume()
+    }
+}
