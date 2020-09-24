6932be8d33733a95aa3d7c8de3595ab35e743649
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-24 22:57:21 +0300

    Initial Commit

---
+++ CoffeeOrder/CoffeeOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrderApp.swift
+//  CoffeeOrder
+//
+//  Created by Abdelrahman Mohamed on 24.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
