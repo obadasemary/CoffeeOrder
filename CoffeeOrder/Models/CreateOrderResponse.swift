421f69699425b33fc5ad8f33c87f15a0a02d5fca
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-25 02:32:20 +0300

    Create CreateOrderResponse.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ CoffeeOrder/Models/CreateOrderResponse.swift
@@ -0,0 +1,13 @@
+//
+//  CreateOrderResponse.swift
+//  CoffeeOrder
+//
+//  Created by Abdelrahman Mohamed on 25.09.2020.
+//
+
+import Foundation
+
+struct CreateOrderResponse: Decodable {
+    
+    let success: Bool
+}
