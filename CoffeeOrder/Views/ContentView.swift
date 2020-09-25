9fa71241a019b6033bc0473691c469a97aa87092
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-25 03:47:33 +0300

    Update ContentView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- CoffeeOrder/Views/ContentView.swift
+++ CoffeeOrder/Views/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     // MARK: - PROPERTIES
     
     @ObservedObject private var orderListVM = OrderListViewModel()
+    @State private var showModel: Bool = false
     
     // MARK: - BODY
     
@@ -21,7 +22,37 @@ struct ContentView: View {
             
             OrderListView(orders: self.orderListVM.orders)
                 .navigationBarTitle("Coffee Orders")
+                .navigationBarItems(
+                    leading:
+                        Button(action: {
+                            reloadOrders()
+                        }, label: {
+                            Image(systemName: "arrow.clockwise")
+                                .foregroundColor(.pink)
+                        }),
+                    trailing:
+                        Button(action: {
+                            showAddCoffeeOrderView()
+                        }, label: {
+                            Image(systemName: "plus")
+                                .foregroundColor(.pink)
+                        })
+                )
+                .sheet(isPresented: self.$showModel, onDismiss: { reloadOrders() }, content: {
+                    
+                    AddCoffeeOrderView(isPresented: self.$showModel)
+                })
         }
+    }
+    
+    private func reloadOrders() {
+        
+        self.orderListVM.fetchOrders()
+    }
+    
+    private func showAddCoffeeOrderView() {
+        
+        self.showModel = true
     }
 }
 
